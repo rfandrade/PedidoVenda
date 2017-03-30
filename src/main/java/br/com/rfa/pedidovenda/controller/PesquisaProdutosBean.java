2a0cce562bbadb6a8da65146e47fa308a375326f
rfandrade <rfandrade@yahoo.com>
2017-03-30 06:28:12 -0300

    Menu adicionado

--- src/main/java/br/com/rfa/pedidovenda/controller/PesquisaProdutosBean.java
+++ src/main/java/br/com/rfa/pedidovenda/controller/PesquisaProdutosBean.java
@@ -5,22 +5,29 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.view.ViewScoped;
 
 @ManagedBean
-@RequestScoped
+@ViewScoped
 public class PesquisaProdutosBean {
 	
 	private List<Integer> produtosFiltrados;
 	
 	public PesquisaProdutosBean(){
-		produtosFiltrados = new ArrayList<Integer>();
+		produtosFiltrados = new ArrayList<>();
 		for (int i = 0; i < 50; i++) {
 			produtosFiltrados.add(i);
 		}
+		System.out.println(produtosFiltrados.size());
 	}
-	
-	public List<Integer> getPedidosFiltrados(){
+
+	public List<Integer> getProdutosFiltrados() {
 		return produtosFiltrados;
 	}
+
+	public void setProdutosFiltrados(List<Integer> produtosFiltrados) {
+		this.produtosFiltrados = produtosFiltrados;
+	}
 	
+
 }
