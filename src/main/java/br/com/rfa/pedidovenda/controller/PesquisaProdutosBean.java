febf1ad79f83c09d5ff8f16c1419a5f609be0849
rfandrade <rfandrade@yahoo.com>
2017-03-30 16:56:26 -0300

    Ajuste no projeto

--- src/main/java/br/com/rfa/pedidovenda/controller/PesquisaProdutosBean.java
+++ src/main/java/br/com/rfa/pedidovenda/controller/PesquisaProdutosBean.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.view.ViewScoped;
 
 @ManagedBean
